db77fa1db1ffa0a983cd9df4c574215f5d471384
nayebuljannath780 <chynayeb780@gmail.com>
2023-04-26 21:35:36 +0600

    services api complete

--- index.js
+++ index.js
@@ -26,25 +26,23 @@ const client = new MongoClient(uri, {
     }
 });
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+async function run() {
+    try {
+        const serviceCollection = client.db('motorMedics').collection('services'); 
 
+        app.get('/', async (req, res) => {
+            const query = {}  // in this query you have to say, what you want to search? if you search for all then give a empty object
+            const cursor = serviceCollection.find(query); // for find have to take a cursor. And find is doing base of query.
+            const services = await cursor.toArray(); // promise convert to an array for using client site.
+            res.send(services);
+        })
+    }
+    finally {
 
+    }
 
-
-
-
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
