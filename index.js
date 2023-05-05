3607218f6eb28c9676d3e2721e9b9f78c1f1e5f0
nayebuljannath780 <chynayeb780@gmail.com>
2023-05-05 23:45:54 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -9,13 +9,8 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.crbp5cr.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,15 +21,50 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
     try {
-        const serviceCollection = client.db('motorMedics').collection('services'); 
+        const serviceCollection = client.db('motorMedics').collection('services'); //create database & collection
+        const productsCollection = client.db('motorMedics').collection('products');
+        const orderCollection = client.db('motorMedics').collection('orders');
 
-        app.get('/', async (req, res) => {
-            const query = {}  // in this query you have to say, what you want to search? if you search for all then give a empty object
+        //services api
+        app.get('/services', async (req, res) => {
+            const query = {};  // in this query you have to say, what you want to search? if you search for all then give a empty object
             const cursor = serviceCollection.find(query); // for find have to take a cursor. And find is doing base of query.
             const services = await cursor.toArray(); // promise convert to an array for using client site.
             res.send(services);
+        });
+
+        // products api
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        // service by id
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }; // in mongodb database _id have ObjectId form.
+            const service = await serviceCollection.findOne(query); // use findOne method for finding a specific item
+            res.send(service);
+        });
+
+        // product by id
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }; // in mongodb database _id have ObjectId form.
+            const product = await productsCollection.findOne(query); // use findOne method for finding a specific item
+            res.send(product);
+        });
+
+        // orders api
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
     }
     finally {
